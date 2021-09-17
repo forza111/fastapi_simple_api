a46539f3504d8c300965542db7c547974f53a207
forza111 <n94i3k6forza>
2021-09-17 09:38:11 +0300

    Running with docker

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
